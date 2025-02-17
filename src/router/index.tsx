92f7411876fbd293d000bd7b09c551eb304b6aa2
kawan <kawanchaudhary@gmail.com>
2025-02-18 01:34:01 +0530

    fixed bugs

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import * as ROUTE_PATH from './config'
 import Countries from '../Screens/Countries'
 import Weather from '../Screens/Weather'
@@ -10,6 +10,7 @@ const AppRouter: React.FC = () => {
       <Routes>
         <Route path={ROUTE_PATH.COUNTRIES_PATH} element={<Countries />} />
         <Route path={ROUTE_PATH.WEATHER_PATH} element={<Weather />} />
+        <Route path="*" element={<Navigate to={ROUTE_PATH.COUNTRIES_PATH} />} />
       </Routes>
     </Router>
   );
